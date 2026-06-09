34e747beabd90e1874dd2bc6e5b10961a8e0eec8
Feomzie <joyogboh@gmail.com>
2026-06-09 21:14:21 +0100

    feat: Add get_by_id method to UserRepository for user retrieval by ID

--- app/repositories/user_repo.py
+++ app/repositories/user_repo.py
@@ -6,6 +6,10 @@ class UserRepository:
     @staticmethod
     def get_by_email(db, email):
         return db.query(User).filter(User.email == email).first()
+
+    @staticmethod
+    def get_by_id(db, user_id):
+        return db.query(User).filter(User.id == user_id).first()
     
     @staticmethod
     def create(db, user):
